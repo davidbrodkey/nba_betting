ac18c1f649f9b71edcf06cf7f0755cec5dee0d23
davidbrodkey <davidbrodkey@gmail.com>
2020-12-31 22:58:35 -0800

    fixed read in, added random, added color

--- nba_betting_shiny/app.R
+++ nba_betting_shiny/app.R
@@ -18,12 +18,12 @@ ui <- fluidPage(
         ,
         pickerInput(inputId="x_axis"
                     ,label="X Axis Variable"
-                    ,choices=c("self_elo_prob","self_carm.elo_prob","self_raptor_prob","bet_decimal","winnings")
+                    ,choices=c("self_elo_prob","self_carm.elo_prob","self_raptor_prob","bet_decimal","winnings","random")
                     ,selected="self_elo_prob")
         ,
         pickerInput(inputId="y_axis"
                     ,label="Y Axis Variable"
-                    ,choices=c("self_elo_prob","self_carm.elo_prob","self_raptor_prob","bet_decimal","winnings")
+                    ,choices=c("self_elo_prob","self_carm.elo_prob","self_raptor_prob","bet_decimal","winnings","random")
                     ,selected="bet_decimal")
         
         
@@ -37,20 +37,26 @@ ui <- fluidPage(
 
 server <- function(input, output) {
    
-  raw_df<-data.frame(read_csv("~/Desktop/nba_betting/nba_betting_shiny/preped_nba_betting_odds.csv"))
+  raw_df<-data.frame(read_csv("~/Desktop/nba_betting/nba_betting_shiny/preped_nba_betting_odds.csv",
+                              col_types=cols(self_carm.elo_prob="n"
+                                             ,self_raptor_prob="n")))
   
   
   filt_df<-reactive({
     df<-raw_df
-    
     df<-df[df$season %in% input$selected_season,]
+    
+    df$bet_result<-ifelse(df$winnings>0,"Good Bet","Bad Bet")
+    df$random<-runif(nrow(df))
     
     df
   })
   
    output$scatter_plot <- renderPlot({
+     df<-filt_df()
      
-     ggplot(data=filt_df(),aes_string(x=input$x_axis,y=input$y_axis))+
+     
+     ggplot(data=df,aes_string(x=input$x_axis,y=input$y_axis,col="bet_result"))+
        geom_point()
    })
    
